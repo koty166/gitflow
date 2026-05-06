d5d67267b6a43d1b5fef5a1f8037b20e5b01d7fd
Константин <Konstantin.Bragin@softline.com>
2026-05-06 16:21:46 +0300

    #2 add some testss

--- tests/test_calculator.py
+++ tests/test_calculator.py
@@ -4,3 +4,5 @@ def test_multiply():
     assert multiply(3, 4) == 12
     assert multiply(-2, 5) == -10
     assert multiply(0, 100) == 0
+    assert multiply(0, -100) == 0
+    assert multiply(-100, -100) == 100000
